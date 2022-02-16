56392ec4cf6cd6529464c04c4526fc1c30834581
Facundo <facundogds@gmail.com>
2022-02-16 22:27:13 +0000

    Completo

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = domainGenerator();
+  });
   console.log("Hello Rigo from the console!");
 };
 
@@ -14,6 +16,12 @@ let domainGenerator = () => {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
+  let domain = [".com", ".us", ".net", ".io"];
 
   let índicePronoun = Math.floor(Math.random() * pronoun.length);
+  let índiceAdj = Math.floor(Math.random() * adj.length);
+  let índiceNoun = Math.floor(Math.random() * noun.length);
+  let índiceDomain = Math.floor(Math.random() * domain.length);
+
+  return `${pronoun[índicePronoun]}${adj[índiceAdj]}${noun[índiceNoun]}${domain[índiceDomain]}`;
 };
